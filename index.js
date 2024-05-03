d07b3e6d57145f89ca2a16ee685191272e72aa73
Hemanth Kumar <hemanthhm693@gmail.com>
2024-05-03 22:58:00 +0530

    I Guess my final commit

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 function randomgenerate()
 {   const small=document.getElementById("alpha");
     const capital=document.getElementById("alpha2");
-    const number=document.getElementById("Number");
+    const numbers=document.getElementById("Number");
     const special=document.getElementById("Specialchar");
     const output=document.getElementById("output");
+    const input=document.getElementById("input");
     let usable="";
+    let password="";
 
     if(small.checked)
-       usasble+="abcdefghijklmnopqrstuvwxyz";
+       usable+="abcdefghijklmnopqrstuvwxyz";
     if(capital.checked)
         usable+="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if(number.checked)
+    if(numbers.checked)
         usable+="0123456789";
     if(special.checked)
         usable+="%$^&*()!@#";
-
+    if(!capital.checked&&!number.checked&&!special.checked&&!small.checked)
+        window.alert("select the requsites");
     
+    for(let i=0;i<input.value;i++)
+    {    password+=usable[Math.floor(Math.random()*usable.length)];
+    }
 
+    output.value=password;
 }
